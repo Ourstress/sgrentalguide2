ad2d5ce779c7afde97def51f4b3529085f48446d
SG <sgng1@e.ntu.edu.sg>
2019-01-21 15:50:38 +0000

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people</h1>
+    <p style={{ color: `purple`, fontSize: `36px` }}>Hello world</p>
+    <p>Now go build something great.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+    <br />
+    <Link to="/About/">Go to About</Link>
+  </Layout>
+)
+
+export default IndexPage
