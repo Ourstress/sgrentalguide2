4199f79cbe41f36778da9623fc0a7aabd8298355
SG <sgng1@e.ntu.edu.sg>
2019-01-25 03:21:59 +0000

    added scss and css modules and worked on styling tutorial

---
+++ src/components/header/header.js
@@ -0,0 +1,45 @@
+// @flow
+
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import styles from './header.module.scss'
+
+type SiteInfoType = {
+  siteTitle: String,
+}
+
+const Header = ({ siteTitle }: { siteTitle: SiteInfoType }) => (
+  <div className={styles.header}>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
+
+export default Header
