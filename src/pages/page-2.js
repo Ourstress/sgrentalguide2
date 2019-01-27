202ff56847ef28fa6c7e0534a0b72935f28330c7
SG <sgng1@e.ntu.edu.sg>
2019-01-27 11:13:21 +0000

    added transformer markdown plugin and some markdown files

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,63 @@
+// @flow
+
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, StaticQuery } from 'gatsby'
 
 import Layout from '../components/layout/layout'
 import SEO from '../components/seo'
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+type dataQuery = {
+  allMarkdownRemark: {
+    totalCount: number,
+    edges: node[],
+  },
+}
+type node = {
+  node: {
+    id: string,
+    frontmatter: frontmatter,
+    excerpt: string,
+  },
+}
+type frontmatter = {
+  title: string,
+  date: string,
+}
+
+const SecondPage = ({ data }: { data: dataQuery }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+          totalCount
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date(formatString: "DD MMMM, YYYY")
+              }
+              excerpt
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <SEO title="Pandas" />
+        <h1>All about happy pandas</h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>{node.frontmatter.title}</h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )}
+  />
 )
 
 export default SecondPage
