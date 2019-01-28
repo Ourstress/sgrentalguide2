0ce8e8efe4116ca7f07b113fdb2038c927ff8d58
SG <sgng1@e.ntu.edu.sg>
2019-01-28 11:23:44 +0000

    added template for new pages, fixed bug by installing earlier gatsby

---
+++ src/templates/blog-post.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout/layout'
+
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+
+// Doesn't work because staticQuery cannot take variables
+// when gatsby-node passes graphql variables to a component, the component is treated as a page and can run page query 
+// export default ({ data }) => (
+//   <StaticQuery
+//     query={graphql`
+//       query($slug: String!) {
+//         markdownRemark(fields: { slug: { eq: $slug } }) {
+//           html
+//           frontmatter {
+//             title
+//           }
+//         }
+//       }
+//     `}
+//     render={data => (
+//       <Layout>
+//         <div>
+//           <h1>{post.frontmatter.title}</h1>
+//           <div dangerouslySetInnerHTML={{ __html: post.html }} />
+//         </div>
+//       </Layout>
+//     )}
+//   />
+// )
