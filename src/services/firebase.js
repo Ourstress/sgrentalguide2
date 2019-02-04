c641f62f62130d5853e2474bea9df7261502a0dc
SG <sgng1@e.ntu.edu.sg>
2019-02-04 08:34:19 +0000

    added firebase config

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import app from 'firebase/app'
+// Initialize Firebase
+const config = {
+  apiKey: process.env.GATSBY_FIREBASE_APIKEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASEURL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECTID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGINGSENDERID,
+}
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+  }
+}
+export default Firebase
